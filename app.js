96d5dedaf534a98867ec19556d03c09b7ac2b40d
SUYOUNG KIM <SUYOUNG KIM>
2017-03-06 19:15:01 +0900

    seperated intents

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ const Article = require('./article');
 const Words = require('./word');
 
 var urls = ['http://www.wsj.com/xml/rss/3_7085.xml',
-              'https://news.google.com/news?cf=all&ned=us&hl=en&topic=h&num=3&output=rss',
-              // 'https://news.google.com/news?ned=us&hl=en&topic=po&output=rss',
-              'https://news.google.com/news?ned=us&hl=en&topic=w&output=rss',
+              // 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories',
+              // 'http://feeds.chron.com/houstonchronicle/topheadlines',
+              // 'http://online.wsj.com/xml/rss/3_7198.xml',
+              // 'http://www.sfgate.com/rss/collectionRss/News-Category-RSS-Feeds-14708.php',
+              'http://feeds.cbsnews.com/CBSNewsMain?format=xml'
             ];
 var parser = new xml2js.Parser();
 var words = new Words.Words();
+var articles = [];
 
 for (var i in urls) {
   var url = urls[i];
-  var reponse = request('GET', url);
+  var response = request('GET', url);
 
-  parser.parseString(reponse.body, function(err, result) {
+  parser.parseString(response.body, function(err, result) {
     for (var i in result.rss.channel[0].item) {
       var item = result.rss.channel[0].item[i];
       var article = new Article.Article();
       article.setupFormItem(item);
+      articles.push(article);
       words.parseArticle(article);
     }
   });
@@ -32,5 +36,8 @@ var top = words.getTopN(10)[0];
 
 console.log("-----------------");
 console.log(top.word);
-console.log(top.articles);
+for (var i in top.articles) {
+  var article = top.articles[i];
+  console.log(article.title);
+}
 console.log("---------------------");
--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 "use strict";
 
 const Alexa = require('alexa-sdk');
-const xml2js = require('xml2js');
-const request = require('sync-request');
+// const xml2js = require('xml2js');
+// const request = require('sync-request');
 const Article = require('./article');
 const Words = require('./word');
+
+const urls = ['http://www.wsj.com/xml/rss/3_7085.xml',
+// 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories',
+// 'http://feeds.chron.com/houstonchronicle/topheadlines',
+// 'http://online.wsj.com/xml/rss/3_7198.xml',
+// 'http://www.sfgate.com/rss/collectionRss/News-Category-RSS-Feeds-14708.php',
+'http://feeds.cbsnews.com/CBSNewsMain?format=xml'
+];
 
 exports.handler = function(event, context, callback) {
   var alexa = Alexa.handler(event, context);
@@ -14,10 +22,93 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
-    'HelloWorldIntent': function () {
-      var url = 'http://www.wsj.com/xml/rss/3_7085.xml';
-      var parser = new xml2js.Parser();
-      var reponse = request('POST', url);
-      this.emit(':tell', 'Hello World!');
+  'HelloWorldIntent': function () {
+    this.emit(':tell', 'Parsing news articles');
+
+    var url = 'http://www.wsj.com/xml/rss/3_7085.xml';
+    var parser = new xml2js.Parser();
+    var reponse = request('POST', url);
+    var articles = [];
+
+    parser.parseString(reponse.body, function(err, result) {
+      for (var i in result.rss.channel[0].item) {
+        var item = result.rss.channel[0].item[i];
+        var article = new Article.Article();
+        article.setupFormItem(item);
+        articles.push(article);
+        // words.parseArticle(article);
+      }
+    });
+
+    var top = words.getTopN(10)[0];
+
+    this.emit(':tell', 'Completed!');
+    // this.emit(':tell', top.word + " is hottest keyword");
+    this.emit(':tell', 'and this is related articles');
+
+    for (var i in articles) {
+      var article = articles[i];
+      this.emit(':tell', article.title);
     }
+  },
+  'StartIntent' : function() {
+    // this.emit(':tell', 'Getting data from server');
+    this.emit('GetDataIntent');
+  },
+  'GetDataIntent': function() {
+    var request = require('sync-request');
+    var responses = [];
+
+    for (var i in urls) {
+      responses.push(request('GET', url));
+    }
+
+    this.attributes['responses'] = responses;
+    this.emit(':tell', 'gotcha');
+    this.emit(':saveState', true);
+  },
+  'XMLParsingIntent' : function () {
+    var xml2js = require('xml2js');
+    var parser = new xml2js.Parser();
+    var articles = [];
+
+    parser.parseString(reponse.body, function(err, result) {
+      for (var i in result.rss.channel[0].item) {
+        var item = result.rss.channel[0].item[i];
+        var article = new Article.Article();
+        article.setupFormItem(item);
+        articles.push(article);
+        // words.parseArticle(article);
+      }
+    });
+
+    this.attributes['articles'] = articles;
+    this.emit(':saveState', true);
+  },
+  'ArticleParsingIntent' : function () {
+    var articles = this.attributes['articles'];
+    var words = new Words.Words();
+
+    for (var i in articles) {
+      words.parseArticle(articles[i]);
+    }
+
+    this.attributes['words'] = words;
+    this.emit(':saveState', true);
+  },
+  'TopNIntent' : function() {
+    var words = this.attributes['words'];
+
+    this.attributes['top_article'] = words.getTopN(10)[0];
+    this.emit(':saveState', true);
+  },
+  'TellIntent' : function() {
+
+    for (var i in articles) {
+      var article = articles[i];
+      this.emit(':tell', article.title);
+    }
+
+    this.emit(':tell', 'Parsing news articles');
+  }
 };
